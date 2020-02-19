e300e9324a4d7e03afc7ba937d106b572212e3ef
Arkadyuti Bandyopadhyay <bandyopadhyayarkadyuti@gmail.com>
2020-02-19 11:35:16 +0530

    Changed the styling for the first page

--- pages/splash.js
+++ pages/splash.js
@@ -28,9 +28,9 @@ export default class Splash extends React.Component {
     return (
       <View style={styles.container}>
         <Text style={{ fontWeight: 'bold', fontSize: 20, paddingBottom: '15%' }}>Click here to navigate to the respective screens</Text>
-        <TouchableOpacity style= {{ padding: '10%', backgroundColor: 'blue', opacity: 0.7}} onPress={() => {this.props.navigation.navigate('List' , { data: this.state.data});}}><Text>Move to list</Text></TouchableOpacity>
+        <TouchableOpacity style= {{ padding: '10%', backgroundColor: 'blue', opacity: 0.7, width: '60%'}} onPress={() => {this.props.navigation.navigate('List' , { data: this.state.data});}}><Text style={styles.size}>Move to list</Text></TouchableOpacity>
         <Text style={{padding: '5%'}}>&nbsp;</Text>
-        <TouchableOpacity style= {{ padding: '10%', backgroundColor: 'blue', opacity: 0.7}} onPress={() => {this.props.navigation.navigate('Search' , { data: this.state.data});}}><Text>Move to search</Text></TouchableOpacity>
+        <TouchableOpacity style= {{ padding: '10%', backgroundColor: 'blue', opacity: 0.7, width: '60%'}} onPress={() => {this.props.navigation.navigate('Search' , { data: this.state.data});}}><Text style={styles.size}>Move to search</Text></TouchableOpacity>
       </View>
     );
   }
@@ -42,5 +42,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  size: {
+    fontSize: 20,
+    alignSelf: "center"
   }
 });
